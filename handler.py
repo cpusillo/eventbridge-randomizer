765bb4d6cc2261751e371a8901251b0f21c56246
Corrine Pusillo <cpusillo@gmail.com>
2023-07-13 10:17:17 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,14 +1,18 @@
 import boto3, random
 
 def main(event, context):
+    # Call the EventBridge Scheduler client from boto3 pkg.
     client = boto3.client('scheduler')
 
+    # Randomize minutes, hours, and days based on preferences/work day.
     minutes = random.choice(['00', '15', '30', '45'])
     hour = random.choice(['10', '11', '12', '13', '14', '15', '16', '17', '18'])
     day = random.choice(['MON','TUE', 'WED', 'THU', 'FRI'])
 
+    # Build the cron expression in AWS's format
     cron_expression = str(f'cron({minutes} {hour} ? * {day} *)')
 
+    # Actually call the update_schedule method from boto3.client
     client.update_schedule(
         Name="name-of-event-schedule",
         FlexibleTimeWindow = {"Mode": "OFF"},
